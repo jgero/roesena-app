19d255f5434f2f81f9947af54c0eefc2c1c359ee
Johannes Gerold <johannes@gerold-online.de>
2019-10-19 16:05:47 +0200

    updating and creating articles

--- middleware/src/index.ts
+++ middleware/src/index.ts
@@ -4,7 +4,7 @@ import { buildSchema } from "graphql";
 import fs from 'fs';
 
 import { getAuthLevel } from './context';
-import { article, articles } from './article/articleResolvers';
+import { article, articles, newArticle, updateArticle, deleteArticle } from './article';
 import { me, login, logout, changePw } from './auth/authResolver';
 import { image, images, newImage, updateImage, deleteImage } from './image';
 import { person, persons } from './person/personResolvers';
@@ -37,7 +37,10 @@ import { person, persons } from './person/personResolvers';
         image,
         newImage,
         updateImage,
-        deleteImage
+        deleteImage,
+        newArticle,
+        updateArticle,
+        deleteArticle
       },
       graphiql: true,
       // the context contains the authLevel of the user from the current cookie, aswell as the request and the response
