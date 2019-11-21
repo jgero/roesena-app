31c839e4f6ef10b0b26494431bcc8b976580fdb6
Johannes Gerold <johannes.gerold.ext@gmail.com>
2019-11-22 00:07:28 +0100

    list subitems work

--- frontend/src/app/editing/editing.component.ts
+++ frontend/src/app/editing/editing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { trigger, transition, query, style, animate } from '@angular/animations';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { GroupsGQL } from '../GraphQL/query-services/all-groups-gql.service';
 import { map } from 'rxjs/operators';
-import { PersonsGQL } from '../GraphQL/query-services/all-persons-gql.service';
+import { EventsShallowGQL } from '../GraphQL/query-services/events/all-events-shallow-gql.service';
+import { ShallowArticlesGQL } from '../GraphQL/query-services/articles/all-articles-shallow-gql.service';
 
 @Component({
   selector: 'app-editing',
@@ -34,16 +35,29 @@ export class EditingComponent {
     {
       title: 'Personen',
       path: 'persons',
-      list: this.personsGql.watch().valueChanges.pipe(map(el => el.data.persons.map(per => ({ _id: per._id, value: per.name }))))
+      list: of([])
     },
     {
       title: 'Gruppen',
       path: 'groups',
       list: this.groupsGql.watch().valueChanges.pipe(map(el => el.data.groups.map(grp => ({ _id: grp._id, value: grp.name }))))
+    },
+    {
+      title: 'Events',
+      path: 'events',
+      list: this.eventsGql.watch().valueChanges.pipe(map(el => el.data.events.map(el => ({ _id: el._id, value: el.title }))))
+    },
+    {
+      title: 'Artikel',
+      path: 'articles',
+      list: this.articlesGql.watch().valueChanges.pipe(map(el => el.data.articles.map(el => ({ _id: el._id, value: el.title }))))
+    },
+    {
+      title: 'Bilder',
+      path: 'images',
+      list: of([])
     }
   ];
 
-  constructor(private personsGql: PersonsGQL, private groupsGql: GroupsGQL) {
-    // this.groupList = ;
-  }
+  constructor(private groupsGql: GroupsGQL, private eventsGql: EventsShallowGQL, private articlesGql: ShallowArticlesGQL) {}
 }
