20e847bbc9a1c805e7ecc6864e914a0653154a93
Johannes Gerold <johannes.gerold.ext@gmail.com>
2019-11-25 23:20:19 +0100

    fixed strange length of undefined bug in editing sublist search

--- frontend/src/app/editing/list-filter.pipe.ts
+++ frontend/src/app/editing/list-filter.pipe.ts
@@ -5,6 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ListFilterPipe implements PipeTransform {
   transform(list: { _id: string; value: string }[], searchString: string): { _id: string; value: string }[] {
-    return list.filter(elem => elem.value.toLowerCase().includes(searchString.toLowerCase()));
+    if (list) {
+      return list.filter(elem => elem.value.toLowerCase().includes(searchString.toLowerCase()));
+    } else {
+      return [];
+    }
   }
 }
