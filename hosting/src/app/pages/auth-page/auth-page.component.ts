5b92bd39b7de3bfed400ac220d4fe43132073b00
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-03-04 21:01:20 +0100

    profile and auth page progress

--- hosting/src/app/pages/auth-page/auth-page.component.ts
+++ hosting/src/app/pages/auth-page/auth-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy } from "@angular/core";
 import { Router } from "@angular/router";
-import { Subscription } from "rxjs";
+import { Subscription, Observable } from "rxjs";
 
 import { AuthService } from "../../services/auth.service";
 import { LoadingService } from "src/app/shared/services/loading.service";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-auth-page",
@@ -11,27 +13,53 @@ import { LoadingService } from "src/app/shared/services/loading.service";
   styleUrls: ["./auth-page.component.scss"]
 })
 export class AuthPageComponent {
-  newName: string;
-  constructor(public auth: AuthService, private router: Router, private loading: LoadingService) {}
+  constructor(public auth: AuthService, private firestore: AngularFirestore, private loading: LoadingService) {}
+  private persons: Observable<{ name: string; id: string; authLevel: number }[]>;
 
-  public logout() {
+  public get $persons(): Observable<{ name: string; id: string; authLevel: number }[]> {
+    if (this.persons) return this.persons;
+    this.persons = this.firestore
+      .collection<{ name: string; authLevel: number }>("persons")
+      .snapshotChanges()
+      .pipe(
+        map(action =>
+          action.map(val => ({
+            name: val.payload.doc.data().name,
+            id: val.payload.doc.id,
+            authLevel: val.payload.doc.data().authLevel
+          }))
+        )
+      );
+    return this.persons;
+  }
+
+  public updateName(val: any) {
     this.loading.$isLoading.next(true);
-    this.auth.logout().subscribe({
-      next: _ => {
-        this.loading.$isLoading.next(false);
-        this.router.navigate(["auth", "login"]);
-      },
-      error: err => {
-        this.loading.$isLoading.next(false);
-        console.log(err);
-      }
+    this.auth.updateName(val.newName).subscribe({
+      next: () => this.loading.$isLoading.next(false),
+      error: err => console.log(err)
     });
   }
 
-  public updateName() {
-    this.auth.updateName(this.newName).subscribe({
-      next: _ => console.log("updated!"),
-      error: err => console.log(err)
-    });
+  public getAuthLevelText(): string {
+    const user = this.auth.$user.getValue();
+    if (user) {
+      switch (user.authLevel) {
+        case 0:
+          return "Gast";
+        case 1:
+          return "Mitglied";
+        case 2:
+          return "Gruppenleiter/Elferrat";
+        case 3:
+          return "Präsidium";
+        case 4:
+          return "Admin";
+        default:
+          return "Fehler";
+      }
+    } else {
+      return "";
+    }
   }
 }
