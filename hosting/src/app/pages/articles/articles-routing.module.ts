050b2522b68e5cdf8ff21f3dbb20957a4e93db54
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-03-25 16:04:54 +0100

    overview page

--- hosting/src/app/pages/articles/articles-routing.module.ts
+++ hosting/src/app/pages/articles/articles-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "overview" },
   { path: "overview", component: OverviewComponent },
   { path: "detail/:id", component: DetailComponent },
-  { path: "editor", component: EditorComponent },
-  { path: "editor/:id", component: EditorComponent }
+  { path: "edit", component: EditorComponent },
+  { path: "edit/:id", component: EditorComponent }
 ];
 
 @NgModule({
--- hosting/src/app/shared/cards/cards.module.ts
+++ hosting/src/app/shared/cards/cards.module.ts
@@ -7,8 +7,9 @@ import { ConvertersModule } from "../converters/converters.module";
 import { EventCardComponent } from "./event-card/event-card.component";
 import { ImageCardComponent } from "./image-card/image-card.component";
 import { ArticleCardComponent } from "./article-card/article-card.component";
+import { TagsComponent } from "./tags/tags.component";
 
-const components = [EventCardComponent, ImageCardComponent, ArticleCardComponent];
+const components = [EventCardComponent, ImageCardComponent, ArticleCardComponent, TagsComponent];
 
 @NgModule({
   declarations: components,
