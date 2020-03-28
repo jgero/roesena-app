855c465ba149278702ad1202b043260f18dbf10e
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-03-28 19:03:45 +0100

    search by link and first two group pages fillout

--- hosting/src/app/shared/filters/images-by-search-string/filter-images-by-search-string.pipe.ts
+++ hosting/src/app/shared/filters/images-by-search-string/filter-images-by-search-string.pipe.ts
@@ -8,10 +8,11 @@ import { appImage } from "src/app/utils/interfaces";
 export class FilterImagesBySearchStringPipe implements PipeTransform {
   transform(values: appImage[], searchString: string): appImage[] {
     if (!Array.isArray(values) || !searchString) return values;
-    return values.filter(
-      img =>
-        img.title.toLowerCase().includes(searchString.toLowerCase()) ||
-        img.tags.some(tag => tag.toLowerCase().includes(searchString.toLowerCase()))
-    );
+    return values.filter(img => {
+      // create a regex for all the words in the search string
+      const regexes = searchString.split(" ").map(word => new RegExp(word.toLowerCase()));
+      // check if every regex is in at least one of the tags
+      return regexes.every(exp => img.tags.some(tag => exp.test(tag.toLowerCase())));
+    });
   }
 }
