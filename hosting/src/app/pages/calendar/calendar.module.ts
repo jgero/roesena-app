df3bd7f3d28d2707da02207ff42e6d1b3d17b252
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-04-20 19:02:00 +0200

    calendar with day zoom-in on click, fixes #60

--- hosting/src/app/pages/calendar/calendar.module.ts
+++ hosting/src/app/pages/calendar/calendar.module.ts
@@ -8,9 +8,22 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
   declarations: [CalendarComponent, DayComponent],
-  imports: [CommonModule, CalendarRoutingModule, MatToolbarModule, MatButtonModule, MatIconModule, MatGridListModule],
+  imports: [
+    CommonModule,
+    CalendarRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatBadgeModule,
+    MatListModule,
+  ],
 })
 export class CalendarModule {}
