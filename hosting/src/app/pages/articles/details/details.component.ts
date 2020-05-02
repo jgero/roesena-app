c25035549af910d6230667d3a5396351cfba1605
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-05-02 22:39:00 +0200

    more resolver usage and details cleanup

--- hosting/src/app/pages/articles/details/details.component.ts
+++ hosting/src/app/pages/articles/details/details.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 
-import { ArticleDalService } from "src/app/services/DAL/article-dal.service";
 import { AuthService } from "src/app/services/auth.service";
-import { appArticle, appImage, appElement } from "src/app/utils/interfaces";
+import { appArticle, appImage } from "src/app/utils/interfaces";
 import { Details } from "src/app/utils/ui-abstractions";
 import { ImageDalService } from "src/app/services/DAL/image-dal.service";
 import { map } from "rxjs/operators";
@@ -14,21 +13,17 @@ import { map } from "rxjs/operators";
   templateUrl: "./details.component.html",
   styleUrls: ["./details.component.scss"],
 })
-export class DetailsComponent {
+export class DetailsComponent extends Details {
   article: appArticle;
   $image: Observable<appImage>;
 
-  constructor(imageDAO: ImageDalService, private auth: AuthService, route: ActivatedRoute) {
+  constructor(imageDAO: ImageDalService, auth: AuthService, route: ActivatedRoute) {
+    super(auth);
     this.article = route.snapshot.data.article;
     this.$image = imageDAO.getBySearchStrings(this.article.tags, 1).pipe(map((imgs) => imgs[0]));
   }
 
   getLinkToImages(val: appArticle): string {
     return `/images/overview/${val.tags.join(",")}`;
-  }
-
-  canEdit(appEl: appElement): boolean {
-    const user = this.auth.$user.getValue();
-    return user && (user.id === appEl.ownerId || user.groups.includes("admin"));
   }
 }
