31d76b9abeab46f23dfbc58ef2f3086e8b66232e
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-05-08 15:57:15 +0200

    tag document updating on article write for #115

--- functions/src/aggregation-denormalization/articles.ts
+++ functions/src/aggregation-denormalization/articles.ts
@@ -1,23 +1,63 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-export const articleWriteListener = functions.firestore.document("articles/{articleId}").onWrite((change) => {
+export const articleWriteListener = functions.firestore.document('articles/{articleId}').onWrite(async (change) => {
   if (!change.before.exists) {
-    // New document Created : add one to count
-    return admin
+    // New document Created : add one to article count
+    await admin
       .firestore()
-      .collection("meta")
-      .doc("articles")
+      .collection('meta')
+      .doc('articles')
       .update({ amount: admin.firestore.FieldValue.increment(1) });
+
+    // map tags element to increment 1
+    const tags: { [key: string]: any } = (change.after.data() as any).tags as { [key: string]: boolean };
+    for (const key in tags) {
+      if (tags.hasOwnProperty(key)) {
+        tags[key] = admin.firestore.FieldValue.increment(1);
+      }
+    }
+    // run increment update operation on tag meta document
+    await admin.firestore().collection('meta').doc('tags').update(tags);
+    return true;
   } else if (change.before.exists && change.after.exists) {
-    // Updating existing document : Do nothing
+    // Updating existing document : update tag counter
+    const tagsBefore = (change.before.data() as any).tags as { [key: string]: boolean };
+    const tagsAfter = (change.after.data() as any).tags as { [key: string]: boolean };
+    const tagsUpdateOp: { [key: string]: any } = {};
+    // tags that were there before and not after -> got removed -> increment -1
+    for (const key in tagsBefore) {
+      if (tagsBefore.hasOwnProperty(key) && !tagsAfter[key]) {
+        tagsUpdateOp[key] = admin.firestore.FieldValue.increment(-1);
+      }
+    }
+    // tags that weren't there before, but after -> got added -> increment 1
+    for (const key in tagsAfter) {
+      if (tagsAfter.hasOwnProperty(key) && !tagsBefore[key]) {
+        tagsUpdateOp[key] = admin.firestore.FieldValue.increment(1);
+      }
+    }
+    // run increment update operation on tag meta document
+    await admin.firestore().collection('meta').doc('tags').update(tagsUpdateOp);
+    return true;
   } else if (!change.after.exists) {
     // Deleting document : subtract one from count
-    return admin
+    await admin
       .firestore()
-      .collection("meta")
-      .doc("articles")
+      .collection('meta')
+      .doc('articles')
       .update({ amount: admin.firestore.FieldValue.increment(-1) });
+
+    // map tags element to increment -1
+    const tags: { [key: string]: any } = (change.before.data() as any).tags as { [key: string]: boolean };
+    for (const key in tags) {
+      if (tags.hasOwnProperty(key)) {
+        tags[key] = admin.firestore.FieldValue.increment(-1);
+      }
+    }
+    // run increment update operation on tag meta document
+    await admin.firestore().collection('meta').doc('tags').update(tags);
+    return true;
   }
   return false;
 });
