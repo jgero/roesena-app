9b21794c45e13609ec3a15b0aabf4e01399d59e2
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-06-18 19:37:08 +0200

    loading indication for articles and popup delete confirmation

---
+++ hosting/src/app/state/global.effects.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Effect, Actions } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { filter, tap } from 'rxjs/operators';
+
+@Injectable()
+export class GlobalEffects {
+  @Effect({ dispatch: false })
+  globalErrorHandler$ = this.actions$.pipe(
+    tap((action) => console.log(action.type.toLowerCase())),
+    filter((action) => new RegExp('.*((error)|(failure)|(failed)).*').test(action.type.toLowerCase())),
+    tap((action) => this.snackbar.open(((action as any).payload.error as Error).message, 'OK'))
+  );
+
+  constructor(private actions$: Actions<Action>, private snackbar: MatSnackBar) {}
+}
