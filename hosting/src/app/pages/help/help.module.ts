17d20fd3a1d89c55e3028e5ba463841c53ecb1f7
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-09-30 18:19:33 +0200

    help page with page fragment links

--- hosting/src/app/pages/help/help.module.ts
+++ hosting/src/app/pages/help/help.module.ts
@@ -5,9 +5,10 @@ import { HelpRoutingModule } from './help-routing.module';
 import { HelpComponent } from './help.component';
 import { HeadingsModule } from '@shared/headings/headings.module';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HelpComponent],
-  imports: [CommonModule, HelpRoutingModule, HeadingsModule, MatIconModule],
+  imports: [CommonModule, HelpRoutingModule, HeadingsModule, MatIconModule, MatButtonModule],
 })
 export class HelpModule {}
