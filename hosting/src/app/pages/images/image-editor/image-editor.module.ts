27881d03cfe450df097482c0c8b991df8acc7938
Johannes Gerold <johannes.gerold.ext@gmail.com>
2020-10-01 16:11:17 +0200

    popup with important hints for creating content and some other popup stuff

--- hosting/src/app/pages/images/image-editor/image-editor.module.ts
+++ hosting/src/app/pages/images/image-editor/image-editor.module.ts
@@ -6,7 +6,7 @@ import { StoreModule } from '@ngrx/store';
 import * as fromImage from '../../../state/images/editor/reducers/image.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { ImageEffects } from '../../../state/images/editor/effects/image.effects';
-import { EditorComponent, DeleteDialogComponent } from './editor.component';
+import { EditorComponent } from './editor.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -17,7 +17,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [EditorComponent, DeleteDialogComponent],
+  declarations: [EditorComponent],
   imports: [
     CommonModule,
     ImageEditorRoutingModule,
