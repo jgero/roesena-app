767e81c31843692ec532666539d89366868e27df
jgero <johannes.gerold.ext@gmail.com>
2021-01-06 17:52:57 +0100

    new untested image conversion

--- functions/src/image-storage-management/image-editing.ts
+++ functions/src/image-storage-management/image-editing.ts
@@ -1,54 +1,73 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as sharp from 'sharp';
 
-const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-export const cropImage = functions
+const THUMB_WIDTH = 300;
+const THUMB_HEIGHT = 200;
+const FULL_WIDTH = 800;
+const FULL_HEIGHT = 550;
+
+export const convertImage = functions
   .region('europe-west1')
   .storage.object()
   .onFinalize(async (object) => {
-    const fileBucket = object.bucket; // The Storage bucket that contains the file.
-    const filePath = object.name; // File path in the bucket.
-    const contentType = object.contentType; // File content type.
+    // the storage bucket that contains the file
+    const fileBucket = object.bucket;
+    // file path in the bucket
+    const filePath = object.name;
+    // name of the file
+    const fileName =  path.basename(filePath);
+    // file content type
+    const contentType = object.contentType;
 
-    // Exit if this is triggered on a file that is not an image.
+    // exit if this is triggered on a file that is not an image
     if (!fileBucket || !filePath || !contentType || !contentType.startsWith('image/')) {
       console.log('This is not an image.');
-      return false;
+      return null;
     }
 
-    // Get the file name.
-    const fileName: string = path.basename(filePath);
-    // Exit if the image is already a thumbnail.
-    if (fileName.endsWith('_cropped')) {
-      console.log('Already cropped.');
-      return false;
+    // exit if the image is not in the tmp directory
+    if (!filePath.startsWith('tmp/')) {
+      console.log('not a fresh upload');
+      return null;
     }
 
-    // Download file from bucket.
+    // download file from bucket
     const bucket = admin.storage().bucket(fileBucket);
     const tempFilePath = path.join(os.tmpdir(), fileName);
-    const metadata = {
-      contentType: contentType,
-    };
     await bucket.file(filePath).download({ destination: tempFilePath });
-    console.log('Image downloaded locally to', tempFilePath);
-    // resize the image to have the desired size
-    await spawn('convert', [tempFilePath, '-resize', '800x550^', '-gravity', 'center', '-extent', '800x550', tempFilePath]);
-    console.log('Image resized and saved to', tempFilePath);
-    // append '_cropped' the file name to be able to tell what files do not have to be touched again
-    const thumbFileName = `${fileName}_cropped`;
-    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
-    // Uploading the resized image
-    await bucket.upload(tempFilePath, {
-      destination: thumbFilePath,
-      metadata: metadata,
-    });
-    // delete the original file in the bucket
+
+    // metadata for upload streams
+    const metadata = {
+      contentType: 'image/webp',
+    };
+
+    // convert and write thumbnail
+    // thumbs get a prefix
+    const thumbFilePath = `thumbnails/thumb_${fileName}`;
+    // create write stream for uploading thumbnail
+    const thumbnailUploadStream = bucket.file(thumbFilePath).createWriteStream({metadata});
+    // resize and convert to webp and pipe into upload stream
+    const toThumbnailTransformer = sharp().resize({ width: THUMB_WIDTH, height: THUMB_HEIGHT, fit: 'cover', position: 'centre' }).webp().pipe(thumbnailUploadStream);
+    // read stream from temp fs into transformer
+    await bucket.file(tempFilePath).createReadStream().pipe(toThumbnailTransformer);
+
+    // convert and write full sized image
+    const fullImageFilePath = `fullsized/${fileName}`;
+    // create write stream for uploading full sized image
+    const fullImageUploadStream = bucket.file(fullImageFilePath).createWriteStream({metadata});
+    // resize and convert to webp and pipe into upload stream
+    const toFullImageTransformer = sharp().resize({ width: FULL_WIDTH, height: FULL_HEIGHT, fit: 'cover', position: 'centre' }).webp().pipe(fullImageUploadStream);
+    // read stream from temp fs into transformer
+    await bucket.file(tempFilePath).createReadStream().pipe(toFullImageTransformer);
+
+    // delete original image in the bucket
     await bucket.file(filePath).delete();
-    // Once the thumbnail has been uploaded delete the local file to free up disk space.
-    return fs.unlinkSync(tempFilePath);
+    // clean up temp fs
+    await fs.unlinkSync(tempFilePath);
+    return null;
   });
