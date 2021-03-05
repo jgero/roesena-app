a8b83c0016ee08aa803ac392e0a08152b6a584b9
jgero <33359660+jgero@users.noreply.github.com>
2021-03-05 16:58:38 +0100

    random pictures on startpage tiles

--- hosting/src/app/pages/start-page/start-page.component.ts
+++ hosting/src/app/pages/start-page/start-page.component.ts
@@ -5,6 +5,7 @@ import { SubscriptionService } from '@services/subscription.service';
 import { SeoService } from '@services/seo.service';
 import { forStartpage as eventForStartpage, LoadAllEvents } from '@state/events';
 import { forStartpage as articleForStartpage, LoadArticlePage } from '@state/articles';
+import { LoadStartPage } from '@state/images';
 
 @Component({
   selector: 'app-start-page',
@@ -12,16 +13,24 @@ import { forStartpage as articleForStartpage, LoadArticlePage } from '@state/art
   styleUrls: ['./start-page.component.scss'],
 })
 export class StartPageComponent implements OnInit, OnDestroy {
+  images$ = this.store.select('images', 'startpageTiles');
   event$ = this.store.select(eventForStartpage);
   article$ = this.store.select(articleForStartpage);
+
+  public readonly tilesPerRow = 2;
 
   constructor(private store: Store<State>, private subs: SubscriptionService, seo: SeoService) {
     seo.setTags(undefined, 'Webseite der Röhlinger Sechtanarren', undefined, '/');
   }
 
+  getRowStyle(index: number): boolean {
+    return Math.floor(index / this.tilesPerRow) % 2 === 1;
+  }
+
   ngOnInit() {
-    this.store.dispatch(new LoadArticlePage({ limit: 1 }));
-    this.store.dispatch(new LoadAllEvents());
+    this.store.dispatch(new LoadStartPage({ tileAmount: 4 }));
+    //this.store.dispatch(new LoadArticlePage({ limit: 1 }));
+    //this.store.dispatch(new LoadAllEvents());
   }
 
   ngOnDestroy() {
---
+++ hosting/src/app/utils/interfaces/tile-element.ts
@@ -0,0 +1,6 @@
+export interface TileElement {
+  heading: string;
+  subheading: string;
+  imageUrls: string[];
+  pageLink: string;
+}
